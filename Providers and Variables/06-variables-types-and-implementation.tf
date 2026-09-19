34bf57a03991f2a591d9bb8a4a90feb48461c592
SKILL UP WITH STAN <109236992+skillupwithstan@users.noreply.github.com>
2026-09-19 10:22:39 +0530

    Update 06-variables-types-and-implementation.tf

--- Providers and Variables/06-variables-types-and-implementation.tf
+++ Providers and Variables/06-variables-types-and-implementation.tf
@@ -1,14 +1,17 @@
 1. Types of Terraform Variables
+
 There are two types of variables in Terraform -
 
 Simple values
 Collection Variable
+
 1.1 Simple Values variables
 As the name suggests Simple Values variables are which hold only a single value. Here the types of Simple Value variables -
 
 string
 number
 bool
+
 1.2 Collection Variable
 In the collection variable, it consists of -
 
@@ -268,39 +271,3 @@ variable "project_environment" {
   }
 }
 bash
-
-# Variables Demo
-
-```hcl
-
-# Define an input variable for the EC2 instance type
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
-# Define an input variable for the EC2 instance AMI ID
-variable "ami_id" {
-  description = "EC2 AMI ID"
-  type        = string
-}
-
-# Configure the AWS provider using the input variables
-provider "aws" {
-  region      = "us-east-1"
-}
-
-# Create an EC2 instance using the input variables
-resource "aws_instance" "example_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-}
-
-# Define an output variable to expose the public IP address of the EC2 instance
-output "public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.example_instance.public_ip
-}
-
-```
